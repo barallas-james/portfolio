eb29b59369aed49f38bbf62462dd704793a156d8
James Barallas <barallas.james@gmail.com>
2019-03-18 20:00:27 +0800

    New Portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+$(document).ready(function(){
+    $('.sidenav').sidenav();
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
+    $(window).on('scroll', function(){
+		if ($(window).scrollTop()) {
+			$('nav').addClass('black');
+			$('nav').removeClass('transparent');
+		}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+		else {
+			$('nav').removeClass('black');
+			$('nav').addClass('transparent');
+		}
+	});
+});
